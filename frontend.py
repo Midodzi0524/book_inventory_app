cdb96cd6c1c04bc590f949efa246f06379c0d93a
Maurice Attiogbe <mauriceattiogbe96@gmail.com>
2023-04-09 10:32:00 +0000

    OOP- backend.py

--- frontend.py
+++ frontend.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import customtkinter
-import backend
+from backend import Database
+
+db= Database()
 
 def get_selected_row(event):
     global selected_tuple
@@ -18,25 +20,25 @@ def get_selected_row(event):
 
 def view_command():
     list1.delete(0,END)
-    for row in backend.view():
+    for row in db.view():
         list1.insert(END,row)
 
 def search_command():
     list1.delete(0,END)
-    for row in backend.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
+    for row in db.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
         list1.insert(END,row)
         
 def add_command():
-    backend.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+    db.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
     list1.delete(0,END)
     list1.insert(END,(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()))
     
 def delete_command():
-    backend.delete(selected_tuple[0])
+    db.delete(selected_tuple[0])
 
 
 def update_command():
-    backend.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+    db.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
          
 
 customtkinter.set_appearance_mode('light')
